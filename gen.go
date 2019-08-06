975f15ee3622918cc5e58bd80111eb5ac0b395b0
Jacob Moody <jsmoody@iastate.edu>
2019-08-06 17:52:03 -0500

    Clean gen.go

--- gen.go
+++ gen.go
@@ -4,6 +4,7 @@ import (
 	"crypto"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 
 	"golang.org/x/crypto/openpgp"
@@ -11,64 +12,67 @@ import (
 	"golang.org/x/crypto/openpgp/packet"
 )
 
-func exitOnError(err error, where string) {
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR upon %s:\n%v\n", where, err)
-		os.Exit(1)
-	}
-}
-
 func main() {
+	log.SetFlags(0)
 	var name = flag.String("n", "", "Full name")
 	var comment = flag.String("c", "", "Comment")
 	var mail = flag.String("m", "", "E-Mail address")
-	var keyname string
-	var help = flag.Bool("help", false, "View this help message and exit.")
 
 	flag.Parse()
-	if *help || flag.NArg() != 1 {
-		fmt.Fprintln(os.Stderr, "Usage: pgp/gen [-m <mail>] [-n <full name>] [-c <comment>] <key file name>")
+	if flag.NArg() != 1 {
+		fmt.Fprintf(os.Stderr, "Usage: %s [-m <mail>] [-n <full name>] [-c <comment>] <key file name>\n", os.Args[0])
 		os.Exit(1)
 	}
-	keyname = flag.Arg(0)
+	keyname := flag.Arg(0)
 
 	cfg := &packet.Config{
 		DefaultHash: crypto.SHA256,
 	}
 
-	var keyPair *openpgp.Entity
 	keyPair, err := openpgp.NewEntity(*name, *comment, *mail, cfg)
-	exitOnError(err, "creating key pair")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// sign all key identities
 	for _, id := range keyPair.Identities {
-		err := id.SelfSignature.SignUserId(id.UserId.Id, keyPair.PrimaryKey, keyPair.PrivateKey, nil)
-		exitOnError(err, "signing identity")
+		err = id.SelfSignature.SignUserId(id.UserId.Id, keyPair.PrimaryKey, keyPair.PrivateKey, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	pub, err := os.Create(keyname + ".pub")
-	exitOnError(err, "creating public key file")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	armorWriter, err := armor.Encode(pub, openpgp.PublicKeyType, nil)
-	exitOnError(err, "opening public armor encoder")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	err = keyPair.Serialize(armorWriter)
-	exitOnError(err, "serializing public key")
-	err = armorWriter.Close()
-	exitOnError(err, "closing public armor encoder")
-	err = pub.Close()
-	exitOnError(err, "closing public key file")
+	if err != nil {
+		log.Fatal(err)
+	}
+	armorWriter.Close()
+	pub.Close()
 
 	priv, err := os.Create(keyname + ".priv")
-	exitOnError(err, "creating private key file")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	armorWriter, err = armor.Encode(priv, openpgp.PrivateKeyType, nil)
-	exitOnError(err, "opening private armor encoder")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	err = keyPair.SerializePrivate(armorWriter, nil)
-	exitOnError(err, "serializing private key")
-	err = armorWriter.Close()
-	exitOnError(err, "closing private armor encoder")
-	err = priv.Close()
-	exitOnError(err, "closing private key file")
+	if err != nil {
+		log.Fatal(err)
+	}
+	armorWriter.Close()
+	priv.Close()
 }
