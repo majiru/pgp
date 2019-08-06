79de866839490865ec74daeadc6b1def67e55ebc
Jacob Moody <jsmoody@iastate.edu>
2019-08-06 17:01:02 -0500

    Cleanup sign.go

--- sign.go
+++ sign.go
@@ -1,28 +1,18 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	"io"
-	"io/ioutil"
+	"log"
 	"os"
 
 	"golang.org/x/crypto/openpgp"
 )
 
-func exitOnError(err error, where string) {
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR upon %s:\n%v\n", where, err)
-		os.Exit(1)
-	}
-}
-
 func main() {
-	var privFile *os.File
-	var err error
-
 	var armor = flag.Bool("a", false, "armor")
+	log.SetFlags(0)
 
 	flag.Parse()
 	if flag.NArg() != 1 {
@@ -30,18 +20,18 @@ func main() {
 		os.Exit(0)
 	}
 
-	privFile, err = os.Open(flag.Arg(0))
-	exitOnError(err, "opening private key")
+	privFile, err := os.Open(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	privBytes, err := ioutil.ReadAll(privFile)
-	exitOnError(err, "reading private key file")
-
-	entityList, err := openpgp.ReadArmoredKeyRing(bytes.NewBuffer(privBytes))
+	entityList, err := openpgp.ReadArmoredKeyRing(privFile)
 	if err == io.EOF {
-		entityList, err = openpgp.ReadKeyRing(bytes.NewBuffer(privBytes))
-		exitOnError(err, "reading private key (decoding armor failed with EOF)")
-	} else {
-		exitOnError(err, "reading private key")
+		privFile.Seek(0, io.SeekStart)
+		entityList, err = openpgp.ReadKeyRing(privFile)
+	}
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	if *armor {
@@ -49,5 +39,7 @@ func main() {
 	} else {
 		err = openpgp.DetachSign(os.Stdout, entityList[0], os.Stdin, nil)
 	}
-	exitOnError(err, "signing data")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
