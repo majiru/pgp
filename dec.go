2a977efa54712746c593ca656fac727fd8016113
Jacob Moody <jsmoody@iastate.edu>
2019-08-06 19:43:15 -0500

    Clean dec.go

--- dec.go
+++ dec.go
@@ -5,67 +5,56 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"os"
 
 	"golang.org/x/crypto/openpgp"
 	"golang.org/x/crypto/openpgp/armor"
 )
 
-func exitOnError(err error, where string) {
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR upon %s:\n%v\n", where, err)
-		os.Exit(1)
-	}
-}
-
 func main() {
-	var privFile *os.File
-	var err error
-
-	armorFlag := len(os.Args) == 3 && os.Args[1] == "-a"
-	if len(os.Args) != 2 && !armorFlag {
-		fmt.Fprintln(os.Stderr, "Usage: pgp/dec [-a] <private key>")
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <private key>\n", os.Args[0])
 		os.Exit(1)
 	}
 
-	if armorFlag {
-		privFile, err = os.Open(os.Args[2])
-	} else {
-		privFile, err = os.Open(os.Args[1])
+	privFile, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
 	}
-	exitOnError(err, "opening private key")
 
-	privBytes, err := ioutil.ReadAll(privFile)
-	exitOnError(err, "reading private key file")
-
-	entityList, err := openpgp.ReadArmoredKeyRing(bytes.NewBuffer(privBytes))
+	entityList, err := openpgp.ReadArmoredKeyRing(privFile)
 	if err == io.EOF {
-		entityList, err = openpgp.ReadKeyRing(bytes.NewBuffer(privBytes))
-		exitOnError(err, "reading private key (decoding armor failed with EOF)")
-	} else {
-		exitOnError(err, "reading private key")
+		privFile.Seek(0, io.SeekStart)
+		entityList, err = openpgp.ReadKeyRing(privFile)
+	}
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	stdinBytes, err := ioutil.ReadAll(os.Stdin)
-	exitOnError(err, "reading stdin")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	var cipherReader io.Reader
-	cipherReader = bytes.NewReader(stdinBytes)
-	block, err := armor.Decode(cipherReader)
-	if err == nil {
+	block, err := armor.Decode(bytes.NewReader(stdinBytes))
+	switch err {
+	default:
+		log.Fatal(err)
+	case nil:
 		cipherReader = block.Body
-	} else if err == io.EOF {
+	case io.EOF:
 		cipherReader = bytes.NewReader(stdinBytes)
-	} else {
-		exitOnError(err, "decoding message armor")
 	}
 
 	mesg, err := openpgp.ReadMessage(cipherReader, entityList, nil, nil)
-	exitOnError(err, "decrypting message")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	bytes, err := ioutil.ReadAll(mesg.UnverifiedBody)
-	exitOnError(err, "reading decrypted message")
-
-	_, err = os.Stdout.Write(bytes)
-	exitOnError(err, "writing to stdout")
+	_, err = io.Copy(os.Stdout, mesg.UnverifiedBody)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
